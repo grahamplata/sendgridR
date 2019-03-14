eef74a6d8db6faa1179fb694d13a84bd6a179134
Graham Plata <graham.plata@gmail.com>
2019-03-14 15:25:03 -0400

    email attachments

--- R/sendgrid.R
+++ R/sendgrid.R
@@ -7,19 +7,19 @@
 # html - string (content of email)
 # attachment - base64enc string
 
-sendgrid <- function(api, to, subject, from, text, 
-                     html=NULL, cc=NULL, bcc=NULL, reply_to=NULL, attachment=NULL) {
-  
+sendgrid <- function(api, to, subject, from, text,
+                     html = NULL, cc = NULL, bcc = NULL, reply_to = NULL, attachment = NULL, filename = NULL) {
+
   json <- '{ "personalizations": [ { "to": ['
   json <- paste0(json, sendgrid_json_emails(to))
   json <- paste0(json, '], "subject": "', subject, '" } ], "from": ')
   json <- paste0(json, sendgrid_json_emails(from))
-  json <- paste0(json, ', "content": [ { "type": "text/plain", "value": "' ,text, '" }] }')
+  json <- paste0(json, ', "content": [ { "type": "text/plain", "value": "', text, '" }]')
+  json <- paste0(json, ', "attachments": [ { "type": "text/csv", "content": "', attachment, '", "filename": "', filename, '" }] }')
 
-  curl_args <- paste0('-X "POST" "https://api.sendgrid.com/v3/mail/send" -H "Authorization: Bearer ', 
-                      api,'" -H "Content-Type: application/json" -d ')
+  curl_args <- paste0('-X "POST" "https://api.sendgrid.com/v3/mail/send" -H "Authorization: Bearer ', api, '" -H "Content-Type: application/json" -d ')
   curl_args <- paste0(curl_args, "'", json, "'")
-  sys_ret <- system2("curl", curl_args, stdout=FALSE, stderr=FALSE)
-  
-  return(sys_ret)  
+  sys_ret <- system2("curl", curl_args, stdout = FALSE, stderr = FALSE)
+
+  return(sys_ret)
 }
